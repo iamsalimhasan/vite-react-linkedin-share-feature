112efcfbec4aac7778d9c165a39365b23a91d515
salim hasan <salimhasan.co.in@gmail.com>
2025-09-03 11:32:19 +0530

    fix bug
    fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { SnackbarProvider } from "notistack";
+import { LinkedinShareButton } from "react-share";
 
 function App() {
   return (
     // comment
     <SnackbarProvider autoHideDuration={2000}>
-      <section className="w-full h-full p-4">
+      <LinkedinShareButton
+        url="https://c.ndtvimg.com/2025-06/q3tab73c_salman-khan_625x300_15_June_25.jpg?im=FeatureCrop,algorithm=dnn,width=773,height=435"
+        className="w-full h-full p-4"
+      >
         <p className="rounded-md bg-orange-200 text-black font-semibold text-center py-2 px-4 hover:cursor-pointer">
           Share post
         </p>
-      </section>
+      </LinkedinShareButton>
     </SnackbarProvider>
   );
 }
